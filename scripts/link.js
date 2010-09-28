0ecb8a9f305807f8a7100d0626f22b349a8b54d2
Randall E. Barker <simstorm@mac.com>
2010-09-28 14:58:57 -0700

    Link detection

--- scripts/link.js
+++ scripts/link.js
@@ -1,19 +1,28 @@
 var dmz =
       { data: require("dmz/runtime/data")
       , defs: require("dmz/runtime/definitions")
+      , mask: require("dmz/types/mask")
       , messaging: require("dmz/runtime/messaging")
       , object: require("dmz/components/object")
       , objectType: require("dmz/runtime/objectType")
       , undo: require("dmz/runtime/undo")
       }
+   // Functions
+   , findWhiteList
+   , isLinkable
+   , updateNoLink
+   // Variables
    , firstHandle = dmz.object.create("Tool Link Node")
    , secondHandle = dmz.object.create("Tool Link Node")
    , NetType = dmz.objectType.lookup("Network Node")
    , NetLink = dmz.defs.createNamedHandle("Network Link")
+   , NoLinkState = dmz.defs.lookupState("No Linking")
    , toolLink
    , startNode
-   , isNetNode = false
+   , noLinkObj
+   , typeCache = {}
    ;
+
 
 (function () {
    if (firstHandle) {
@@ -23,6 +32,92 @@ var dmz =
       dmz.object.activate(secondHandle);
    }
 }());
+
+findWhiteList = function (type) {
+
+   var result = typeCache[type.name()]
+     , list
+     ;
+
+   if (!result) {
+
+      list = type.find("link-list.object-type").config().get("link-list.object-type");
+
+      result = [];
+
+      list.forEach(function (config) {
+
+         var type = config.objectType("name");
+         if (type) { result.push(type); }
+         else { self.log.error ("Unknown object type:", config.string("name")); }
+      });
+
+      typeCache[type.name()] = result;
+   }
+
+   return result;
+};
+
+isLinkable = function (source, target) {
+
+   var result = false
+     , stype = dmz.object.type (source)
+     , ttype = dmz.object.type (target)
+     , list
+     ;
+
+   if (stype && ttype) {
+
+      list = findWhiteList (stype);
+
+      if (list && list.some(function (type) { return ttype.isOfType(type); })) {
+
+         result = true;
+      }
+      else {
+
+         list = findWhiteList (ttype);
+
+         if (list && list.some(function (type) { return stype.isOfType(type); })) {
+
+            result = true;
+         }
+      }
+   }
+
+   return result;
+};
+
+updateNoLink = function (handle) {
+
+   var state
+     ;
+
+   if (noLinkObj) {
+
+      state = dmz.object.state(noLinkObj);
+
+      if (state) {
+
+         dmz.object.state(noLinkObj, null, state.unset(NoLinkState));
+      }
+   }
+
+   if (handle && (handle != startNode) && !isLinkable(startNode, handle)) {
+
+      state = dmz.object.state(handle);
+
+      if (!state) { state = dmz.mask.create(); }
+
+      if (state) {
+
+         dmz.object.state(handle, null, state.or(NoLinkState));
+         noLinkObj = handle;
+      }
+      else { noLinkObj = null; }
+   }
+   else { noLinkObj = null; }
+};
 
 dmz.messaging.subscribe(self, "First_Link_Object_Message", function (data) {
 
@@ -37,7 +132,6 @@ dmz.messaging.subscribe(self, "First_Link_Object_Message", function (data) {
    }
 
    startNode = null;
-   isNetNode = false;
 
    if (dmz.data.isTypeOf(data)) {
 
@@ -45,7 +139,6 @@ dmz.messaging.subscribe(self, "First_Link_Object_Message", function (data) {
 
       if (handle && dmz.object.isObject(handle)) {
 
-         isNetNode = dmz.object.type(handle).isOfType(NetType);
          startNode = handle;
       }
 
@@ -66,6 +159,7 @@ dmz.messaging.subscribe(self, "First_Link_Object_Message", function (data) {
 dmz.messaging.subscribe(self, "Update_Link_Position_Message", function (data) {
 
    var pos
+     , handle
      ;
 
    if (data) {
@@ -75,6 +169,13 @@ dmz.messaging.subscribe(self, "Update_Link_Position_Message", function (data) {
       if (pos && secondHandle) {
 
          dmz.object.position(secondHandle, null, pos);
+      }
+
+      handle = data.handle("object", 0);
+
+      if (handle && (handle != noLinkObj)) {
+
+         updateNoLink(handle);
       }
    }
 });
@@ -98,14 +199,15 @@ dmz.messaging.subscribe(self, "Second_Link_Object_Message", function (data) {
 
       if (handle) {
 
-         if (handle == startNode) {
+//self.log.warn ("isLinkable:", isLinkable(startNode, handle));
+
+         if (handle === startNode) {
 
             startNode = null;
             handle = null;
          }
-         else if (dmz.object.isObject(handle) &&
-               (isNetNode || dmz.object.type(handle).isOfType(NetType)) &&
-               startNode) {
+         else if (dmz.object.isObject(handle) && startNode &&
+               isLinkable(startNode, handle)) {
 
             undo = dmz.undo.startRecord("Create Network Link");
 
@@ -120,11 +222,15 @@ dmz.messaging.subscribe(self, "Second_Link_Object_Message", function (data) {
       }
    }
 
+   updateNoLink();
+
    startNode = null;
-   isNetNode = false;
 });
 
 dmz.messaging.subscribe(self, "Failed_Link_Objects_Message", function () {
+
+   var state
+     ;
 
    if (toolLink) {
 
@@ -132,6 +238,7 @@ dmz.messaging.subscribe(self, "Failed_Link_Objects_Message", function () {
       toolLink = null;
    }
 
+   updateNoLink();
+
    startNode = null;
-   isNetNode = false;
 });
