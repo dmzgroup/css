7c7e2b0e2ab6533e9b7b7b207c576b3497fcf8d3
Randall E. Barker <simstorm@mac.com>
2010-10-13 17:20:47 -0700

    Adding text to canvas objects.

--- scripts/cssConst.js
+++ scripts/cssConst.js
@@ -2,5 +2,5 @@ var util = require("dmz/types/util")
   , defs = require("dmz/runtime/definitions")
   ;
 
-util.defineConst(exports, "NameAttr", defs.createNamedHandle("Device Name"));
+util.defineConst(exports, "NameAttr", defs.createNamedHandle("Name"));
 util.defineConst(exports, "OSAttr", defs.createNamedHandle("Operating System"));
