e0d26ff9aad60a6469aa15f449c20ef3e492f839
Randall E. Barker <simstorm@mac.com>
2010-11-10 13:45:04 -0800

    wip

--- scripts/test.js
+++ scripts/test.js
@@ -1,1 +1,14 @@
+var _attack = require("attackAPI")
+  , count = 0
+  ;
+
 self.log.error("LOADED");
+
+_attack.timeSlice(self, function (time) {
+
+  self.log.error(time);
+
+  count += time;
+
+  if (count > 10) { foo.bar(); }
+});
